a95d6dd098e8dd12b10a01345dedf1ee66c8f73a
RuStyC0der <miller_maers@tutanota.com>
2019-05-29 10:47:31 +0300

    changed code

--- first_bomb/keyboard.ino
+++ first_bomb/keyboard.ino
@@ -1,56 +1,20 @@
-int rows[]={9,8,7,6};   //указываем пины строк
-int cols[]={5,4,3,2};   //и столбцов
+#include <Keypad.h>
 
-void keyboard_init() {
-
-        Serial.begin(9600);
-        Serial.println("Starting...");
-        for (int i=0; i<sizeof(rows); i++){
-          pinMode(rows[i], INPUT);
-        }
-        for (int i=0; i<sizeof(cols); i++){
-          pinMode(cols[i], OUTPUT);
-        }
-}
-
-//массив с соответствиями кодов нажатых кнопок
-char simbols[][2]={
-        {0,'1'},
-        {1,'2'},
-        {2,'3'},
-        {3,'A'},
-        {4,'4'},
-        {5,'5'},
-        {6,'6'},
-        {7,'B'},
-        {8,'7'},
-        {9,'8'},
-        {10,'9'},
-        {11,'C'},
-        {12,'*'},
-        {13,'0'},
-        {14,'#'},
-        {15,'D'}
+const byte ROWS = 4; // число строк клавиатуры
+const byte COLS = 4; // число столбцов клавиатуры
+char hexaKeys[ROWS][COLS] = {
+        {'1','2','3','A'},
+        {'4','5','6','B'},
+        {'7','8','9','C'},
+        {'*','0','#','D'}
 };
 
-char keyboard_get_key() {
+byte rowPins[ROWS] = {31, 33, 35, 37}; // к каким выводам подключаем управление строками
+byte colPins[COLS] = {39, 41, 43,45}; // к каким выводам подключаем управление столбцами
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
-        for(int i=0; i<4; i++) { //цикл для перехода по всем строкам
-                digitalWrite(rows[i], HIGH); //подаем на текущую строку высокий уровень
-                for(int j=0; j<4; j++) { //цикл для переходов по всем столбцам
-                        if(digitalRead(cols[j])==HIGH) { //если уровень высокий, то кнопка нажата
-                                return _FindKey(4*i+j);
-                        }
-                }
-                digitalWrite(rows[i], LOW); //выключаем высокий уровень для пройденной строки
-        }
-}
-//функция возвращающая код нажатой кнопки
-char _FindKey(unsigned int keyCode){
-        for(int i=0; i<16; i++) {
-                if(simbols[i][0]==keyCode) {
-                        return simbols[i][1];
-                }
-        }
-        return '-'; //если код на найден возвращаем ‘-‘
+
+
+char keyboard_get_key() {
+        return customKeypad.getKey();
 }
