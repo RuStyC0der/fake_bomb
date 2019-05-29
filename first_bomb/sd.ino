8686c724773b23ea607922a7a29db08648ebfd73
RuStyC0der <miller_maers@tutanota.com>
2019-05-29 19:19:09 +0300

    added simple example (UNTESTED)

--- first_bomb/sd.ino
+++ first_bomb/sd.ino
@@ -1,6 +1,38 @@
-void card_init(/* arguments */) {
-  /* code */
+#include <SD.h>
+
+File myFile;
+
+void setup()
+{
+        Serial.begin(9600);
+        Serial.print("Initializing SD card...");
+// на Ethernet шилде CS соответствует 4 пину. По умолчанию он установлен в режим output
+// обратите внимание, что если он не используется в качестве CS пина, SS пин на оборудовании
+// (10 на большинстве плат Arduino, 53 на Arduino Mega) надо оставить в режиме output.
+// иначе функции библиотеки SD library не будут работать.
+        pinMode(10, OUTPUT);
+        if (!SD.begin(10)) {
+                Serial.println("initialization failed!");
+                return;
+        }
+        Serial.println("initialization done.");
+// открываем файл для считывания данных:
+        myFile = SD.open("test.txt");
+        if (myFile) {
+                Serial.println("test.txt:");
+// считываем все данные из файла:
+                while (myFile.available()) {
+                        Serial.write(myFile.read());
+                }
+// закрываем файл:
+                myFile.close();
+        } else {
+// если файл не открылся, отображаем сообщение об ошибке:
+                Serial.println("error opening test.txt");
+        }
 }
-void card_load_config(){
+
+void loop()
+{
 
 }
