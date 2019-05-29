772e4a51b1fd4d040fcda5253d23ada7a02c416f
RuStyC0der <miller_maers@tutanota.com>
2019-05-29 19:19:22 +0300

    REFACTORED

--- first_bomb/vibration_sensor.ino
+++ first_bomb/vibration_sensor.ino
@@ -1,9 +1,15 @@
-#define VIBRO_1_PIN 6
+#define VIBRO_1_PIN 8
 #define VIBRO_2_PIN 7
 
-bool vibro_state[2];
+void vibro_init(){
+  pinMode(VIBRO_1_PIN, INPUT);
+  pinMode(VIBRO_2_PIN, INPUT);
+}
 
-void vibro_update(/* arguments */) {
-        vibro_state[0] = digitalRead(VIBRO_1_PIN);
-        vibro_state[1] = digitalRead(VIBRO_2_PIN);
+bool vibro_1(/* arguments */) {
+        return digitalRead(VIBRO_1_PIN);
+}
+
+bool vibro_2(/* arguments */) {
+      return digitalRead(VIBRO_2_PIN);
 }
